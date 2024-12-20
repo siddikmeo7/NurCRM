2ad633d0e9f641eb3077c570448343906b3b6b49
Abubakr <siddikme.o7@gmail.com>
2024-12-20 15:50:16 +0500

    Adding models within API's.

---
+++ NurCRM/serializers.py
@@ -0,0 +1,43 @@
+# serializers.py
+from rest_framework import serializers
+from .models import Product, Client, Transaction, Profile, Category, Colour, Sklad, SkladProduct
+
+class ProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = ['id', 'title', 'index', 'category', 'colour', 'price', 'cost_price', 'sold', 'up_to', 'is_active', 'created_at', 'user']
+
+class ClientSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Client
+        fields = ['id', 'name', 'phone_number', 'email', 'address', 'balance', 'user']
+
+class TransactionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Transaction
+        fields = ['id', 'transaction_type', 'amount', 'comments', 'client', 'user', 'created_at']
+
+class ProfileSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Profile
+        fields = ['id', 'bio', 'profile_picture', 'website', 'date_of_birth', 'user']
+
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = ['id', 'name']
+
+class ColourSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Colour
+        fields = ['id', 'name']
+
+class SkladSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Sklad
+        fields = ['id', 'name']
+
+class SkladProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = SkladProduct
+        fields = ['id', 'sklad', 'product', 'quantity']
