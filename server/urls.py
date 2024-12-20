2c381a032bec89822c24d4e6d68b2a92ab6c2d87
sunatl <ibragimovusmon68@gmail.com>
2024-12-20 09:59:41 +0300

    adding swagger

--- server/urls.py
+++ server/urls.py
@@ -4,7 +4,7 @@ from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 from rest_framework import permissions
 
-schema_view = get_schema_view(
+schema_view = get_schema_view(  
     openapi.Info(
         title="API Documentation",
         default_version="v1",
