05db031df1b20e2a307acce88c5a5f1255298ba2
sunatl <ibragimovusmon68@gmail.com>
2024-12-20 10:30:11 +0300

    Adding Session Authentication

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,35 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from django.contrib.auth import login, logout
+from .serialayzer import RegisterSerializer, LoginSerializer
+from rest_framework.permissions import AllowAny
 
-# Create your views here.
+
+class RegisterView(APIView):
+    permission_classes = [AllowAny]  # Allow everyone to access this view
+
+    def post(self, request):
+        serializer = RegisterSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({"message": "User registered successfully"}, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class LoginView(APIView):
+    permission_classes = [AllowAny]  # Allow everyone to access this view
+
+    def post(self, request):
+        serializer = LoginSerializer(data=request.data)
+        if serializer.is_valid():
+            user = serializer.validated_data
+            login(request, user)  # Start a session for the user
+            return Response({"message": "Login successful"}, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_401_UNAUTHORIZED)
+
+
+class LogoutView(APIView):
+    def post(self, request):
+        logout(request)
+        return Response({"message": "Logged out successfully"}, status=status.HTTP_200_OK)
